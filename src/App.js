21fe5eaf0a8d26e0cb3293183354efdec034be48
Salia Eunice <101419339+saliaeunice@users.noreply.github.com>
2022-03-23 07:38:18 -0700

    react useState

--- src/App.js
+++ src/App.js
@@ -1,45 +1,33 @@
 import ProfileCard from './components/ProfileCard';
 
-import React, { Component } from 'react';
+import React, {useState} from 'react'
 
- class App extends Component {
-constructor() {
-super ();
+ function App() {
+const[writers,setWriters]=useState({
+  loading: false,
+  list:[]
+});
 
-this.handleClick =this.handleClick.bind(this);
-this.state ={
-writers:{
-loading: false,
-list:[]
-}
+const handleClick =()=>{
+  setWriters((previousState)=>({
+    ...previousState,
+    loading:true
+  }))
+setTimeout(async()=>{
+  let resp =await fetch("/writers.json");
+  let result =await resp.json();
+
+  setWriters((previousState)=>({
+    ...previousState,
+    loading:false,
+    list:result
+  }));
+}, 2500)
+
 };
 
-}
-handleClick(){
-this.setState({writers:
-{
-  loading:true
-}});
 
-setTimeout(async()=>{
-  let resp = await fetch("/writers.json");
-  let result = await resp. json();
-  
-
-  this.setState({
-writers:{
-  loading:false,
-  list: result
-}
-  });
-}, 3500);
-}
-
-render() {
-  const {
-    writers:{loading, list}
-  }= this.state;
-  if  (loading){
+  if  (writers.loading){
     return(
       <div>
       <h1>Writers Profile</h1>
@@ -57,13 +45,13 @@ render() {
       <h1>Writers Profile</h1>
       <div className='container'>
       
-    {list.length === 0 ? (
- <div className='car action'>
+    {writers.list.length === 0 ? (
+ <div className='card action'>
  <p className='infoText'>Oops... no writer profile found</p>
-<button className='actionBtn' onClick={this.handleClick}>Get Writers</button>
+<button className='actionBtn' onClick={handleClick}>Get Writers</button>
       </div>
     ):(
-list.map((writer) =>(
+writers.list.map((writer) =>(
   <ProfileCard key={writer.id} writer={writer}/>
 ))
     )}
@@ -73,5 +61,5 @@ list.map((writer) =>(
         </div>
     );
   }
-}
+
 export default App;
